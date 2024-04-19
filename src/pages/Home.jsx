0d0baf02c2744c120ede3a34926045dd85ad2341
Reid Back <reid.backcnmt@gmail.com>
2024-04-19 14:48:59 +1000

    updated my bio section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,12 @@ export default function Home() {
                     <h1 className="display-4" id="pageH1">Reid Back</h1>
                     <img src="../assets/me.png" alt="a picture of me" height='300rem' width='300rem' className="rounded-circle"></img>
                     <p className="text-left">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nulla aperiam rerum deleniti sapiente eius distinctio blanditiis tempora dolore inventore, veritatis odio accusamus? Rem enim cumque modi architecto voluptatibus veniam?
+                        Hey, I'm Reid. A current Nuclear Medicine Technologist and aspiring web developer. After spending the better part of a decade in medicine I have felt stagnated and wanting to branch out and try to use my brain a bit more. I have recently completed a full stack web development bootcamp through the University of Sydney. In these past 6 months I have gone from never writing a line of code to learning and implementing many different technologies. I have worked with express, node.js, mySQL and MongoDB, to React, and many many more.
                     </p>
                     <p className="text-left">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure illo itaque modi commodi odio corporis labore, voluptatum ea assumenda quasi natus unde possimus! Omnis voluptate natus, sequi quasi culpa accusamus!
+                        Take a look at 'My Work' to either see my deployed applications (if there is a front end) or the github repository if it's only a backend project. If you like the look of my work and would like to work with me, feel free to reach out and send me an email or contact me on github or linkedIn!
                     </p>
+                   
                 </div>
             </div>
         </div>
